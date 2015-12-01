2893b07af4a96edb9d4bd6deefc78c72a627f6c7
Adegoke Obasa <goke@cottacush.com>
2015-12-01 09:58:51 +0100

    Test Paypal adapter

--- adapter/index.php
+++ adapter/index.php
@@ -3,3 +3,7 @@
  * Adapter Design Pattern Demonstration
  * @author Adegoke Obasa <goke@cottacush.com>
  */
+
+// Using PayPal Adapter
+$paypal = new PayPalAdapter(new PayPal());
+$paypal->pay(2629);
